483593f081c5bc8f98e6141470b13de1848508b4
Maria Luisa Senica <maria@Marias-Mac-mini.local>
2025-10-08 18:26:07 +0800

    updates

--- public-offer.js
+++ public-offer.js
@@ -21,19 +21,26 @@ const { createApp, ref, onMounted } = Vue
           console.log("API Response:", data)
           
           product.value = data.body || {}
-          
           console.log(data.body)
+
+          fetchPhoto();
           
         } catch (error) {
           console.error("Error fetching products:", error)
         } 
       }
+
+       let doc = doc ? 'true' : 'false';
       
-      const fetchDoc = async () => {
+      const fetchPhoto = async () => {
       	try {
         	const res = await fetch (
-          	`{{plan-api-service-url}}/brochure/download-url?key=documents/PLAN123/brochures/test.pdf&mimeType=application/pdf&expiration=3600`
+          	`https://z5fp7xmfuwwaqhzckxqjcffx4a0pnare.lambda-url.eu-west-2.on.aws/api/brochure/download-url?key=${product.value.offerPhotoS3Key}&mimeType=${product.value.offerPhotoS3MimeType}&expiration=3600`
           )
+
+          const data = await res.json();
+          console.log("API Response:", data)
+          
         } catch (error) {
           	console.error("Error fetching doc:", error)
         }
